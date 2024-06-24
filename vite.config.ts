79561d8c06c90c63bb126e35b25eb5108458955a
Elton Ribeiro <96088869+eltonls@users.noreply.github.com>
2024-06-24 15:54:26 -0300

    Added Reverse Proxy Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [vue()],
+    server: {
+      proxy: {
+        "^/api": {
+          target: env.BASE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  });
+});
