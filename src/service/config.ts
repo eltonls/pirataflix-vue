3da12e5edb38082f7ad53ee3d6561aa26c5da152
Elton Ribeiro <96088869+eltonls@users.noreply.github.com>
2024-06-24 14:58:52 -0300

    add axios and rxjs config (#3)

---
+++ src/service/config.ts
@@ -0,0 +1,35 @@
+// Axios Config
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
+
+function apiConfig(baseUrl: string): AxiosRequestConfig {
+    return {
+        baseURL: baseUrl
+    }
+}
+
+function initAxios(config: AxiosRequestConfig, token?: any): AxiosInstance {
+
+    const defineInstance = axios.create(config);
+    defineInstance.interceptors.request.use(
+        (request: AxiosRequestConfig): any => {
+            return request;
+        },
+        (error) => Promise.reject(error)
+    );
+
+    defineInstance.interceptors.response.use(
+        (response) => response,
+        (error: AxiosError) => {
+            alert(error);
+            return Promise.reject(error);
+        }
+    );
+
+    return defineInstance;
+}
+
+function api(baseURL = "/api", token?: any) {
+    return initAxios(apiConfig(baseURL), token);
+}
+
+export default api;
