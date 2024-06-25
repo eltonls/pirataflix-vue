321d82bd250306ae85590b58ff7166b30eaa1267
Elton <eltonrs1999@gmail.com>
2024-06-25 09:13:52 -0300

    add token config with to use .env

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
 
   return defineConfig({
     plugins: [vue()],
     server: {
       proxy: {
         "^/api": {
-          target: env.BASE_API_URL,
+          target: process.env.VITE_BASE_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
