033e735f44cd83f059ef46e99c366986bfa51c12
Elton Ribeiro <96088869+eltonls@users.noreply.github.com>
2024-06-26 13:58:19 -0300

    Add movie detail view with dynamic templates (#8)

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { createApp } from 'vue'
+import PrimeVue from "primevue/config"
+import Card from "primevue/card"
+import Aura from '@primevue/themes/aura'
+import Button from 'primevue/button'
 import './style.css'
 import App from './App.vue'
 import router from './routes'
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
 
-
-createApp(App).use(PrimeVue,{
-    theme: {
-        preset: Aura
-    }
-}).use(router).mount('#app')
-
+createApp(App)
+    .use(PrimeVue, {
+        theme: {
+            preset: Aura,
+            options: {
+                cssLayer: {
+                    name: "primevue",
+                    order: "tailwind-base, primevue, tailwind-utilities"
+                }
+            }
+        }
+    })
+    .use(router)
+    .component("Button", Button)
+    .component("Card", Card)
+    .mount('#app')
---
+++ src/views/MovieDetail/index.vue
@@ -0,0 +1,47 @@
+<script>
+  export default {
+    props: {
+       movie: {}
+    }
+  }
+</script>
+
+<template>
+  <section
+    :class="bg-[url(`${movie.backdrop_path}`)] bg-center bg-no-repeat bg-cover"
+  >
+    <div
+      class="bg-black bg-opacity-80 w-screen h-screen flex items-center p-10"
+    >
+      <Card class="lg:w-1/3" style="background: rgba(0,0,0, .4);">
+        <template #title class="">
+          <h3 class="text-2xl text-white font-bold tracking-wider mb-1">
+            {{ movie.title }}
+          </h3>
+        </template>
+        <template #content>
+          <div class="flex flex-row text-gray-400 gap-2 mb-3">
+            <p class="" id="ano">{{ movie.release_date }}</p>
+            <p class="text-green-400 brightness-120">
+              {{ movie.vote_count }}
+            </p>
+            <p class="">|</p>
+            <p class="" id="screenTime">{{ movie.runtime }} Min</p>
+          </div>
+          <div class="text-white mb-2">
+            <h4 class="font-bold">{{ movie.tagline }}</h4>
+            <p class="">
+              {{ movie.overview }}
+            </p>
+          </div>
+        </template>
+        <template #footer>
+            <div class="flex gap-4">
+                <Button label="Trailer" icon="pi pi-play" severity="primary" class="text-white bg-red-600 border-none hover:scale-105 transition-all"/>
+                <Button label="Salvar" icon="pi pi-heart" severity="contrast" class="border-none hover:scale-105 transition-all"/>
+            </div> 
+        </template>
+      </Card>
+    </div>
+  </section>
+</template>
