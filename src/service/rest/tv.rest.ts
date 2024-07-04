da0fa6284d2bb89ca8aed877075127064db2cca8
Elton Ribeiro <96088869+eltonls@users.noreply.github.com>
2024-07-04 13:28:07 -0300

    add videoplayer for detail view (#22)

--- src/service/rest/tv.rest.ts
+++ src/service/rest/tv.rest.ts
@@ -2,7 +2,9 @@ import QueryParams from "../../models/QueryParams";
 import api from "../rxjs";
 
 export class TvRest {
+  private _BASE_URL = "/api"
+
   getPopularTvShows(params: QueryParams) {
-    return api.get("/tv/popular", params);
+    return api.get("/tv/popular", this._BASE_URL, params);
   }
 }
