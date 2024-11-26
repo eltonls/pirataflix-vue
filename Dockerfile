9c096183c2f2ce7338a676784a68c9eaf962b9ae
Elton <eltonls@proton.me>
2024-11-26 10:37:59 -0300

    New test

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,23 @@
 FROM node:16-slim AS node-base
-
 WORKDIR /src
 
-#Copiando primeiramente os packages para tirar vantagem do cache do docker
+# Accept build-time variables
+ARG VITE_BASE_URL
+ARG VITE_BEARER_TOKEN
+
+# Set environment variables
+ENV VITE_BASE_URL=$VITE_BASE_URL
+ENV VITE_BEARER_TOKEN=$VITE_BEARER_TOKEN
+
 COPY package*.json ./
-
 RUN npm install
-
 COPY . .
-
 RUN npm run build
 
-#NGINX stage
+# NGINX stage
 FROM nginx:stable-alpine3.19-slim AS production-build
-
 EXPOSE 80
-
 WORKDIR /app
-
 COPY --from=node-base /src/dist /usr/share/nginx/html
-
 ADD /config/default.conf /etc/nginx/conf.d/default.conf
-
 CMD ["nginx", "-g", "daemon off;"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,22 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-
-// https://vitejs.dev/config/
-
 export default defineConfig(({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())}  
+  const env = loadEnv(mode, process.cwd());
 
-  return defineConfig({
+  return {
     plugins: [vue()],
     server: {
       proxy: {
         "^/api": {
-          target: process.env.VITE_BASE_URL,
+          target: env.VITE_BASE_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
     },
     define: {
-      'process.env': process.env
+      'process.env': { ...process.env, ...env }
     }
-  });
+  };
 });
-
